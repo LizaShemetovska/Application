3468fe3f37a82b588122662ecaea120415e1be8e
shemetovskaliza <shemetovskaliza@A33COMP72>
2019-04-04 17:48:02 +0300

    Add Weather TodoList

--- src/TodoListApp.js
+++ src/TodoListApp.js
@@ -1,13 +1,138 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
+let todoItems = [];
+todoItems.push({ index: 1, value: "To do task from Math", done: false });
+todoItems.push({ index: 2, value: "Learn Docker", done: false });
+todoItems.push({ index: 3, value: "Solve all problems in a hole world", done: false });
+todoItems.push({ index: 4, value: "Become older", done: true });
 
-export default class TodoList extends Component{
-render(){
-    return(
-     <div>
-         <h2>My TodoList Components works !!!</h2>
-     </div>
+
+
+
+class TodoList extends Component {
+  render() {
+    var todoItems = this.props.items.map((item, index) => {
+      return (<TodoListItem key={index} item={item} index={index} removeItem={this.props.removeItem} markTodoDone={this.props.markTodoDone} />);
+
+    });
+    return (<ul className="list-group"> {todoItems}</ul>);
+  };
+}
+class TodoListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.onClickClose = this.onClickClose.bind(this);
+    this.onClickDone = this.onClickDone.bind(this);
+  }
+  onClickClose() {
+    var index = parseInt(this.props.index);
+    this.props.removeItem(index);
+  }
+  onClickDone() {
+    let index = parseInt(this.props.index);
+    this.props.markTodoDone(index);
+  }
+  render() {
+    let todoClass = this.props.item.done ? "done" : "undone";
+    return (
+     <div className="block">
+
+        <div className={todoClass}>
+          <span className="glyphicon glyphicon-ok icon"
+            aris-hidden="true"
+            onClick={this.onClickDone}>   </span>
+          {this.props.item.value}  
+           
+          <button type="button" className="delete is-medium"
+            onClick={this.onClickClose}>&times;</button>
+        </div>
+
+        </div>
+    );
+
+  };
+
+}
+class TodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit(event) {
+    event.preventDefault();
+    let newItemValue = this.refs.itemName.value;
+
+    if (newItemValue) {
+      this.props.addItem({ newItemValue });
+      this.refs.form.reset();
+
+    }
+  }
+  render() {
+    return (
+      <form ref="form" onSubmit={this.onSubmit} className="form-inline">
+        <input type="text" ref="itemName" className="form=control" placeholder="add new task.."></input>
+        <button type="submit" className="btn btn-default ">Add</button>
+      </form>
+    );
+
+  };
+}
+class TodoHeader extends Component {
+  render() {
+    return <h1>What do you want to do ? </h1>
+  };
+}
+class TodoApp extends Component {
+  constructor(props) {
+    super(props);
+    this.addItem = this.addItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+    this.markTodoDone = this.markTodoDone.bind(this);
+    this.state = { todoItems: todoItems };
+  }
+  addItem(todoItem) {
+    todoItems.unshift({
+      index: todoItems.length + 1,
+      value: todoItem.newItemValue,
+      done: false
+    });
+    this.setState({ todoItems: todoItems });
+  }
+  removeItem(itemIndex) {
+    todoItems.splice(itemIndex, 1);
+    this.setState({ todoItems: todoItems });
+
+  }
+  markTodoDone(itemIndex) {
+    let task = todoItems[itemIndex];
+    todoItems.splice(itemIndex, 1);
+    task.done = !task.done;
+    task.done ? todoItems.push(task) : todoItems.unshift(task);
+    this.setState({ todoItems: todoItems });
+
+  }
+  render() {
+    return (
+        <div id="main">
+        <TodoHeader />
+        <TodoList items={this.props.initItems} removeItem={this.removeItem} markTodoDone={this.markTodoDone} />
+        <TodoForm addItem={this.addItem} />
+
+      </div>
     );
   }
 }
-// export default Home;
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+        {/* load  */}
+        <TodoApp initItems={todoItems} />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,13 +1,74 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+const PLACES =[
+  {name: "Rivne", zip:"33000"},
+  {name: "Washington" ,zip:"20001"},
+  {name: "Los Angeles" ,zip:"90023"},
+  {name: "Seattle" ,zip:"98101"}
 
-export default class Weather extends Component{
-render(){
+];
+class WeatherService extends Component{
+  constructor(){
+      super ();
+      this.state={weatherData:null};
+  }
+  componentDidMount(){
+    const zip =this.props.zip;
+    const apiURL= "http://api.openweathermap.org/data/2.5/weather?q=" +
+    zip +
+    "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    fetch(apiURL)
+    .then(res=>res.json())
+    .then(json => {this.setState( {weatherData:json})})
+  }
+  render(){
+    const weatherData=this.state.weatherData;
+    if(!weatherData) return <div>Loading...</div>
+    const weather=weatherData.weather[0];
+    const iconURL="http://api.openweathermap.org/img/w/" +weather.icon +".png";
     return(
-     <div>
-         <h2>My Weather Components works !!!</h2>
-     </div>
+      // <div>{JSON.stringify(weatherData)}</div>
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconURL} alt ={weatherData.description}/>
+        </h1>
+        
+<p>Current temp: {weatherData.main.temp}°C</p>
+    <p>Min temp: {weatherData.main.temp_min}°C</p>
+    <p>Max temp: {weatherData.main.temp_max}°C</p>
+    <p>Wind speed: {weatherData.wind.speed} m/sec</p>
+    <p>Pressure: {weatherData.main.pressure} pasc</p>
+  
+    </div>
+    );
+
+  } 
+}
+
+class App extends Component {
+constructor(){
+  super();
+   this.state= {
+     activePlace: 0,
+   };
+}
+  render() {
+    const activePlace = this.state.activePlace;
+    return (
+      <div className="App">
+      <WeatherService zip={PLACES[activePlace].zip} key={activePlace}/>
+      { PLACES.map((place,index)=> (
+         <button className={"button is-rounded"} key={index} onClick={()=> {this.setState({activePlace:index})} }> 
+        {/* (console.log(`Clicked ${index} button`))} > */}
+        {place.name}
+        </button>
+      ))}
+      </div>
     );
   }
 }
-// export default Home;
+
+export default App;
